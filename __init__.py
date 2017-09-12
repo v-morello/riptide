154c9db3c2297a8159b90ce3f8f113f4fea955da
Vincent Morello <vmorello@gmail.com>
2017-09-12 16:32:59 +0100

    Added a new Metadata class to carry observation attributes across all data products. Work in progress.

--- __init__.py
+++ __init__.py
@@ -1,18 +1,25 @@
+### Major classes
 from .time_series import TimeSeries
+from .periodogram import Periodogram
+from .metadata import Metadata
+
+### Major functions
 from .search import ffa_search
 
 from .libffa import (
-    ffa_transform_1d, 
-    ffa_transform_2d, 
+    ffa_transform_1d,
+    ffa_transform_2d,
     generate_signal,
     downsample
     )
 
 __all__ = [
-    'TimeSeries', 
-    'ffa_search', 
-    'ffa_transform_1d', 
-    'ffa_transform_2d', 
-    'generate_signal', 
+    'TimeSeries',
+    'Periodogram',
+    'Metadata',
+    'ffa_search',
+    'ffa_transform_1d',
+    'ffa_transform_2d',
+    'generate_signal',
     'downsample'
     ]
