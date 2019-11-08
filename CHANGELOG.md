ddd0e6647085d581852cc33efd246eedaa833966
Vincent Morello <vmorello@gmail.com>
2019-11-08 12:42:14 +0000

    Docstring for ffa_search()

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## HEAD
+
+### Added
+- Full docstring for `ffa_search()`
+
 ## 0.0.2 - 2019-11-05
 ### Added
 - `riptide` is now properly packaged with a `setup.py` script. Updated installation instructions in `README.md`
--- riptide/search.py
+++ riptide/search.py
@@ -44,25 +44,68 @@ def prune_periodogram(periods, snrs):
     return periods[mask], snrs[mask]
 
 
-
 def ffa_search(tseries, rmed_width=4.0, rmed_minpts=101, period_min=1.0, period_max=30.0, fpmin=8, bins_min=240, bins_max=260, ducy_max=0.20, wtsp=1.5, threads=1):
-    """ Run a FFA search of a TimeSeries.
+    """ 
+    Run a FFA search of a single TimeSeries object, producing its periodogram.
 
-    Parameters:
-    -----------
-        tseries: TimeSeries
-            The time series object to search
-        [... and lots of kwargs]
+    Parameters
+    ----------
+    tseries: TimeSeries
+        The time series object to search
+    period_min: float
+        Minimum period to search in seconds (default: 1.0)
+    period_max: float
+        Maximum period to search in seconds (default: 30.0)
+    fpmin: int
+        Minimum number of signal periods that must fit in the data. In other
+        words, place a cap on period_max equal to DATA_LENGTH / fpmin.
+        (default: 8)
+    bins_min: int
+        Minimum number of phase bins in the folded data. Higher values
+        provide better duty cycle resolution. As the code searches longer trial
+        periods, the data are iteratively downsampled so that the number of 
+        phase bins remains between bins_min and bins_max.
+        (default: 240)
+    bins_max: int
+        Maximum number of phase bins in the folded data. Must be strictly
+        larger than bins_min, preferably > 10% larger to avoid downsampling
+        the input data too many times.
+        (default: 260)
+    wtsp: float
+        Multiplicative factor between consecutive boxcar width trials. The
+        smallest width is always 1 phase bin, and the sequence of width
+        trials is generated with the formula:
+        W(n+1) = max( floor(wtsp x W(n)), W(n) + 1 )
+        wtsp = 1.5 gives the following sequence of width trials (in number of
+        phase bins): 1, 2, 3, 4, 6, 9, 13, 19 ...
+        (default: 1.5)
+    ducy_max: float
+        Maximum duty cycle to optimally search. Limits the maximum width of the
+        boxcar matched filters applied to any given profile. 
+        Example: on a 300 phase bin profile, ducy_max = 0.2 means that no 
+        boxcar filter of width > 60 bins will be applied.
+        (default: 0.2)
+    rmed_width: float
+        The width of the running median filter to subtract from the input data
+        before processing, in seconds 
+        (default: 4.0)
+    rmed_minpts: int
+        The running median is calculated of a time scrunched version of the
+        input data to save time: rmed_minpts is the minimum number of
+        scrunched samples that must fit in the running median window
+        Lower values make the running median calculation less accurate but
+        faster, due to allowing a higher scrunching factor.
+        (default: 101)
 
-    Returns:
-    --------
-        ts: TimeSeries
-            The de-reddened and normalised time series that was actually searched
-        plan: ProcessingPlan
-            The processing plan followed
-        pgram: Periodogram
-            The output of the search, containing a 2D array representing S/N
-            as a function of trial period and trial width.
+    Returns
+    -------
+    ts: TimeSeries
+        The de-reddened and normalised time series that was actually searched
+    plan: ProcessingPlan
+        The processing plan followed
+    pgram: Periodogram
+        The output of the search, containing a 2D array representing S/N
+        as a function of trial period and trial width.
     """
     ### Prepare data: deredden then normalise IN THAT ORDER
     ### 'ts' is the post-processed TimeSeries that will be searched
