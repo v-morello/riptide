7a904d98650f81e57902bf2fe4ad0425e9c12052
Vincent Morello <vmorello@gmail.com>
2023-12-18 21:45:46 +0000

    Only use safe optimisation flags when compiling C++ extensions

--- setup.py
+++ setup.py
@@ -3,6 +3,22 @@ from setuptools import setup
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
+# WARNING: Enabling -freciprocal-math (which is also enabled by
+# -funsafe-math-optimizations or -ffast-math) causes surprising inconsistencies
+# when calculating the size of the output periodogram, and the size of the
+# buffers to store data downsampled by a real-valued factor.
+# In turn, this can cause segmentation faults (code attempts to write past the
+# end of these arrays).
+# The flags below provide the same speedups as -ffast-math, without the risks.
+SAFE_FAST_MATH_FLAGS = [
+    '-fassociative-math',
+    '-fno-math-errno',
+    '-ffinite-math-only',
+    '-fno-rounding-math',
+    '-fno-signed-zeros',
+    '-fno-trapping-math',
+]
+
 # The main interface is through Pybind11Extension.
 # * You can add cxx_std=11/14/17, and then build_ext can be removed.
 # * You can set include_pybind11=false to add the include directory yourself,
@@ -15,7 +31,7 @@ ext_modules = [
     Pybind11Extension(
         'riptide.libcpp',
         sorted(['riptide/cpp/python_bindings.cpp']),
-        extra_compile_args=['-O3', '-ffast-math', '-march=native']
+        extra_compile_args=['-O3', '-march=native'] + SAFE_FAST_MATH_FLAGS
     ),
 ]
 
